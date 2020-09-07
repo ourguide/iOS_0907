505c4414f3e8aa2c116d24fbc4bd7286ce587865
Chansik Yun <chansigi@gmail.com>
2020-09-07 09:21:39 +0900

    .

--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -1,29 +1,42 @@
-
-// 1. 확장자
-//    .m   : ObjC + C
-//    .mm  : ObjC + C++
-
-// 2. 헤더파일
+// 클래스 기본 문법
 #import <Foundation/Foundation.h>
-//    C: #include
-// ObjC: #import (#include + include guard)
- 
-// Command + B: Build(Compile)
-// Command + R: Build(Compile) + Run
-// https://github.com/ourguide/iOS_0907
 
-// 3. ObjC 의 모든 클래스에는 NS라는 접두가 있습니다.
-//  NS: NextStep
-//  => ObjC 언어는 패키지 또는 네임스페이스같은 심볼 충돌을 막기 위한 언어적 기능이 존재하지 않습니다.
+// 1. ObjC는 Smalltalk 언어의 영향을 받았습니다.
+//   => 다른 언어와 용어적인 차이가 있습니다.
+
+// 클래스 선언부 - .h(헤더 파일)
+// Java
+#if 0
+// Object의 자식 클래스로 만들어집니다.
+class Car {
+  int speed;  // private
+}
+#endif
+
+// NSObject: ObjC 언어의 최상위 클래스
+@interface Car : NSObject {
+  // 필드를 정의할 수 있습니다.
+  // - 필드에 접근 지정 레벨의 기본은 protected 입니다.
+@public
+  int _speed;
+@private
+  int _color;
+@protected
+}
+
+@end
+
+// 클래스 정의부 - .m(소스 파일)
+@implementation Car
+
+@end
 
 int main() {
-  // C의 문자열
-  const char* s1 = "Hello, C";
-  printf("%s\n", s1);
- 
-  // ObjC의 문자열 - NSString*
-  //  - ObjC의 모든 객체는 '힙'에 생성됩니다.
-  NSString* s2 = @"Hello, ObjC";
-  NSLog(@"%@", s2);
   
 }
+
+
+
+
+
+
