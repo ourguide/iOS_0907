610c74893dbc215457feacb24930e70ef7c1786c
Chansik Yun <chansigi@gmail.com>
2020-09-07 09:45:52 +0900

    .

---
+++ iOS_0907/2_클래스기본문법.m
@@ -0,0 +1,67 @@
+// 클래스 기본 문법
+#import <Foundation/Foundation.h>
+
+// 1. ObjC는 Smalltalk 언어의 영향을 받았습니다.
+//   => 다른 언어와 용어적인 차이가 있습니다.
+
+// 클래스 선언부 - .h(헤더 파일)
+// Java
+#if 0
+// Object의 자식 클래스로 만들어집니다.
+class Car {
+  int speed;  // private
+}
+#endif
+
+// NSObject: ObjC 언어의 최상위 클래스
+@interface Car : NSObject {
+  // 필드를 정의할 수 있습니다.
+  // - 필드에 접근 지정 레벨의 기본은 protected 입니다.
+@public
+  int _speed;
+@private
+  int _color;
+@protected
+}
+
+// 메소드의 선언부
+- (void)go;
+
+// 클래스 메소드의 선언부
++ (void)foo;
+
+@end
+
+// 클래스 정의부 - .m(소스 파일)
+@implementation Car
+
+// 메소드의 구현부
+- (void)go {
+  printf("go - %d\n", _speed);
+}
+
++ (void)foo {
+  printf("foo\n");
+}
+
+@end
+
+int main() {
+  // Car 클래스에 new 메세지를 보낸다.
+  // - receiver: Car
+  // - new: selector
+  Car* car = [Car new];
+  car->_speed = 42;
+  
+  // car->_color = 100;
+  [car go];
+  [Car foo];
+  
+  printf("%d\n", car->_speed);
+}
+
+
+
+
+
+
--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -1,67 +1,9 @@
-// 클래스 기본 문법
+//
+//  main.m
+//  iOS_0907
+//
+//  Created by Chansik Yun on 2020/09/07.
+//  Copyright © 2020 Chansik Yun. All rights reserved.
+//
+
 #import <Foundation/Foundation.h>
-
-// 1. ObjC는 Smalltalk 언어의 영향을 받았습니다.
-//   => 다른 언어와 용어적인 차이가 있습니다.
-
-// 클래스 선언부 - .h(헤더 파일)
-// Java
-#if 0
-// Object의 자식 클래스로 만들어집니다.
-class Car {
-  int speed;  // private
-}
-#endif
-
-// NSObject: ObjC 언어의 최상위 클래스
-@interface Car : NSObject {
-  // 필드를 정의할 수 있습니다.
-  // - 필드에 접근 지정 레벨의 기본은 protected 입니다.
-@public
-  int _speed;
-@private
-  int _color;
-@protected
-}
-
-// 메소드의 선언부
-- (void)go;
-
-// 클래스 메소드의 선언부
-+ (void)foo;
-
-@end
-
-// 클래스 정의부 - .m(소스 파일)
-@implementation Car
-
-// 메소드의 구현부
-- (void)go {
-  printf("go - %d\n", _speed);
-}
-
-+ (void)foo {
-  printf("foo\n");
-}
-
-@end
-
-int main() {
-  // Car 클래스에 new 메세지를 보낸다.
-  // - receiver: Car
-  // - new: selector
-  Car* car = [Car new];
-  car->_speed = 42;
-  
-  // car->_color = 100;
-  [car go];
-  [Car foo];
-  
-  printf("%d\n", car->_speed);
-}
-
-
-
-
-
-
