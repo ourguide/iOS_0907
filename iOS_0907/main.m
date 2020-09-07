32478a67bec209cc192cfcb326d163d12c28f7b8
Chansik Yun <chansigi@gmail.com>
2020-09-07 09:57:22 +0900

    .

--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -1,9 +1,50 @@
-//
-//  main.m
-//  iOS_0907
-//
-//  Created by Chansik Yun on 2020/09/07.
-//  Copyright © 2020 Chansik Yun. All rights reserved.
-//
+// 초기화 메소드
+//  => 생성자
 
 #import <Foundation/Foundation.h>
+
+@interface Car : NSObject {
+  int _color;
+}
+
+// 초기화 메소드
+- (id)init;
+
+- (int)color;
+
+@end
+
+@implementation Car
+
+- (int)color {
+  return _color;
+}
+
+
+// NULL - #define NULL (void*)0
+// nil  - #define nil  (id)0
+
+// * id: 모든 객체를 참조할 수 있는 타입.
+
+// self에 대한 초기화는 초기화함수에서 이루어집니다.
+- (id)init {
+  // 1. 부모의 초기화 메소드를 명시적으로 호출합니다.
+  self = [super init];
+  
+  // 2. 부모의 초기화 메소드가 실패하지 않았다면, 자신의 필드를 초기화한다.
+  if (self != nil) {
+    _color = 42;
+  }
+  
+  // 3. self를 반환한다.
+  return self;
+}
+
+@end
+
+int main() {
+  Car* car = [Car new];
+  printf("%d\n", [car color]);
+}
+
+
