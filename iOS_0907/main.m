dcef8f9a736c5d1476cc87ee078823ff15e38e54
Chansik Yun <chansigi@gmail.com>
2020-09-07 10:05:14 +0900

    .

--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -10,6 +10,9 @@
 // 초기화 메소드
 - (id)init;
 
+- (id)initWithColor:(int)color;
+
+
 - (int)color;
 
 @end
@@ -40,11 +43,33 @@
   return self;
 }
 
+- (id)initWithColor:(int)color {
+  self = [super init];
+  
+  if (self) {
+    _color = color;
+  }
+  
+  return self;
+}
+
 @end
+
+// new
+//  1) 객체의 메모리를 할당한다.
+//  2) init 함수를 통해 객체의 필드를 초기화한다.
+// * new는 init을 호출하도록 약속되어 있습니다.
+
+// init이 아닌 사용자가 정의한 초기화 메소드를 객체 생성시 사용하기 위해서는,
+// 객체의 메모리 할당과 초기화 메소드를 직접 호출해야 한다.
+//  => 2단계 생성 패턴
 
 int main() {
   Car* car = [Car new];
   printf("%d\n", [car color]);
+  
+  Car* car2 = [[Car alloc] initWithColor:100];
+  printf("%d\n", [car2 color]);  
 }
 
 
