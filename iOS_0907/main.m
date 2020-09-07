bfaa4ead5eab7891e4157e140591f395597cdf00
Chansik Yun <chansigi@gmail.com>
2020-09-07 12:04:14 +0900

    .

--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -18,14 +18,12 @@
 // 정적 팩토리 메소드 => 편의 생성자
 //                  : 내부적으로 지정 초기화 메소드를 통해 객체를 편리하게 생성하는 클래스 메소드
 + (Car*)blueCar {
-  return [[Car alloc] initWithRed:0 green:0 blue:255];
+  return [[[Car alloc] initWithRed:0 green:0 blue:255] autorelease]; // !!
 }
 
 + (Car*)redCar {
-  return [[Car alloc] initWithRed:255 green:0 blue:0];
+  return [[[Car alloc] initWithRed:255 green:0 blue:0] autorelease];
 }
-
-
 
 - (id)initWithRed:(int)red green:(int)green blue:(int)blue {
   self = [super init];
@@ -37,11 +35,24 @@
   return self;
 }
 
+- (void)dealloc {
+  printf("Car 객체 파괴..\n");
+  
+  [super dealloc];
+}
+
 @end
 
+// * 편의 생성자를 통해 생성되는 객체들은 Auto Release Pool을 통해 수명을 관리하자.
+
 int main() {
+  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init]; // pool 생성
+  
   Car* blueCar = [Car blueCar];
   Car* redCar = [Car redCar];
+  
+  [pool release];
+  // pool에 등록된 모든 객체에 대해서, release를 보낸다.
   
   
 #if 0
