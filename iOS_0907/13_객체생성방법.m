db3268eb773729715641b8bf4c06daaf26701cd2
Chansik Yun <chansigi@gmail.com>
2020-09-07 16:22:35 +0900

    .

---
+++ iOS_0907/13_객체생성방법.m
@@ -0,0 +1,50 @@
+
+#import <Foundation/Foundation.h>
+
+// 1. ARC 원리 - 정적(컴파일러) 코드 분석
+//  : 컴파일러가 컴파일 시간에 코드를 분석해서, 참조 계수 관련 코드를 삽입
+
+// 2. 객체 생성 방법
+//  1) alloc + initXXX: 컴파일러가 release를 삽입한다.
+//  2) arrayXXX, stringXXX: 컴파일러가 autorelease를 삽입한다.
+//                         => Auto Release Pool이 파괴되는 시점에 파괴되는 객체
+
+// 3. ARC vs MRC
+//  : ARC가 더 빠르게 동작합니다.
+//  => MRC에서는 method를 통해 참조 계수를 조작합니다.
+//     'ARC에서는 참조 계수를 조작하는 모든 코드가 C로 되어 있습니다.'
+
+@interface Person : NSObject
+@end
+@implementation Person
+
+// Override
+- (NSString *)description {
+  return @"Person - Tom";
+}
+
+@end
+
+
+int main() {
+  @autoreleasepool {
+    
+    // 문자열을 생성하는 3가지 방법
+    NSString* s1 = @"Hello";     // Literal
+    // NSString* s2 = [[NSString alloc] initWithString:@"Hello"];  // alloc + initXXX
+    // NSString* s3 = [NSString stringWithString:@"Hello"];        // 편의 생성자 - @autoreleasepool
+    NSLog(@"%@", s1);
+    
+    NSArray* arr1 = [[NSArray alloc] initWithObjects:@"A", @"B", @"C", nil];
+    // [arr1 release];
+    
+    NSArray* arr2 = [NSArray arrayWithObjects:@"A", @"B", @"C", nil];
+    // [arr2 autorelease];
+    
+    NSLog(@"%@", arr2);
+    
+    Person* person = [Person new];
+    NSLog(@"%@", person);
+    
+  }
+}
--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -1,50 +1,109 @@
 
 #import <Foundation/Foundation.h>
 
-// 1. ARC 원리 - 정적(컴파일러) 코드 분석
-//  : 컴파일러가 컴파일 시간에 코드를 분석해서, 참조 계수 관련 코드를 삽입
+// Class Type
+//  Java, C#: Reflection
+//      ObjC: Introspection
+// => 런타임에 클래스의 정보 등을 탐색하는 목적으로 사용한다.
 
-// 2. 객체 생성 방법
-//  1) alloc + initXXX: 컴파일러가 release를 삽입한다.
-//  2) arrayXXX, stringXXX: 컴파일러가 autorelease를 삽입한다.
-//                         => Auto Release Pool이 파괴되는 시점에 파괴되는 객체
+@interface Car : NSObject
+- (void)go;
+@end
 
-// 3. ARC vs MRC
-//  : ARC가 더 빠르게 동작합니다.
-//  => MRC에서는 method를 통해 참조 계수를 조작합니다.
-//     'ARC에서는 참조 계수를 조작하는 모든 코드가 C로 되어 있습니다.'
+@interface Truck : Car
+@end
+
+@implementation Car
+- (void)go {
+  printf("Car go\n");   // 1
+}
+@end
+
+@implementation Truck
+- (void)go {
+  printf("Truck go\n"); // 2
+}
+@end
 
 @interface Person : NSObject
+// - (void)go;
 @end
+
 @implementation Person
-
-// Override
-- (NSString *)description {
-  return @"Person - Tom";
-}
-
+//- (void)go {
+//  printf("Person go\n");
+//}
 @end
 
+
+// 1. ObjC는 '동적 바인딩'을 합니다.
+//  정적 바인딩: 컴파일러가 어떤 함수를 호출할지 결정하는 것
+//            C, C++
+//  동적 바인딩: 런타임에 어떤 객체 타입인지를 확인해서, 함수를 호출하는 것
+//            Java, C++(virtual)
+
+// 2. 많은 객체 지향 언어들은 상속 관계를 통해 다형성을 구현합니다.
+//  - ObjC, Javascript, Go
+//  => Duck Typing(덕 타이핑)
+//    : 동일한 메소드를 가지고 있다면, 다형성을 구현하는 것이 가능하다.
+
+// 3. [object respondsToSelector:@selector(go)]
+//  - receiver(object)가 go 메시지를 수신할 수 있는지 체크한다.
+
+void go(id object) {
+  if ([object respondsToSelector:@selector(go)])
+    [object go];
+}
 
 int main() {
-  @autoreleasepool {
-    
-    // 문자열을 생성하는 3가지 방법
-    NSString* s1 = @"Hello";     // Literal
-    // NSString* s2 = [[NSString alloc] initWithString:@"Hello"];  // alloc + initXXX
-    // NSString* s3 = [NSString stringWithString:@"Hello"];        // 편의 생성자 - @autoreleasepool
-    NSLog(@"%@", s1);
-    
-    NSArray* arr1 = [[NSArray alloc] initWithObjects:@"A", @"B", @"C", nil];
-    // [arr1 release];
-    
-    NSArray* arr2 = [NSArray arrayWithObjects:@"A", @"B", @"C", nil];
-    // [arr2 autorelease];
-    
-    NSLog(@"%@", arr2);
-    
-    Person* person = [Person new];
-    NSLog(@"%@", person);
-    
+  Car* car = [Truck new];
+  // 1. Class를 얻는 3가지 방법
+  //  1) 타입을 통해서 얻는다.
+  Class c1 = [Truck class];
+  
+  //  2) 객체를 통해서 얻는다.
+  Class c2 = [car class];
+  
+  //  3) 문자열을 통해서 얻을 수 있다.
+  //   => 실패의 가능성이 있습니다.
+  Class c3 = NSClassFromString(@"Car");
+  
+  // 2. Class의 용도
+  //  1) 런타임에 객체의 타입을 확인하는 목적으로 사용합니다.
+  if ([Car class] == [car class]) {
+    printf("Car 타입입니다..\n");
   }
+  
+  if ([Truck class] == [car class]) {
+    printf("Truck 타입입니다..\n");
+  }
+  
+  // ObjC: isKindOfClass
+  if ([car isKindOfClass:[Car class]]) {
+    printf("Car의 자식입니다.\n");
+  }
+  
+  
+  // Java: instanceOf
+  // if (car instanceOf Car) { -> true
+  
+  
+  
+  
+  
+  
+//  go([Car new]);
+//  go([Truck new]);
+//  go([Person new]);
+//
+//  Car* car = [Truck new];  // ?
+//  [car go];
 }
+
+
+// id: Objective C의 객체를 참조하는 타입
+// NSObject* 와는 다릅니다.
+
+// 다형성?
+//  : go라는 메소드를 통해 각각의 객체가 다른 동작을 수행한다.
+
