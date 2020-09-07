b413edd802106db799c0493e24547504cd3510f5
Chansik Yun <chansigi@gmail.com>
2020-09-07 15:37:24 +0900

    .

--- iOS_0907/12_프로퍼티활용.m
+++ iOS_0907/12_프로퍼티활용.m
@@ -15,6 +15,8 @@
 - (NSString*)fullName;
 - (void)setFullName:(NSString*)fullName;
 
+- (void)foo;
+
 @end
 
 // => Effective Objective C
@@ -23,12 +25,36 @@
 //   : 초기화 메소드 안에서 값을 설정할 때는 setter를 이용하는 것이 아니라, 직접 접근하는 것이 좋다.
 //  - 인스턴스 변수의 소유권이 부모인 경우
 //   : 반드시 setter를 이용해야 한다.
+//  - getter가 '지연 초기화'를 구현하고 있다면, 내부에서도 getter를 사용해야 한다.
 
 // 2. 인스턴스 변수가 없어도, 메소드의 이름 규칙이 setter, getter의 이름규칙을 정확히 따른다면,
 //    닷 연산자를 이용할 수 있다.
 //    => Swift 에서는 프로퍼티를 만들 때, 필드가 없는 프로퍼티도 만들 수 있습니다.
 
+@interface Phone : NSObject
+@end
+
+@implementation Phone
+- (void)dealloc {
+  printf("Phone 객체 파괴...\n");
+}
+@end
+
 @implementation Person
+- (void)foo {
+  // printf("%p\n", _phone);
+  
+  // 지연 초기화 변수에 접근할 때는 내부에서도 getter를 이용해야 합니다.
+  printf("%p\n", self.phone);
+}
+
+// 지연 초기화: 처음 접근하는 시점에 생성하는 것
+- (Phone*)phone {
+  if (_phone == nil)
+    _phone = [Phone new];
+  return _phone;
+}
+
 - (NSString*)fullName {
   return [NSString stringWithFormat:@"%@, %@", _firstName, _lastName];
 }
@@ -63,14 +89,6 @@
 
 @end
 
-@interface Phone : NSObject
-@end
-
-@implementation Phone
-- (void)dealloc {
-  printf("Phone 객체 파괴...\n");
-}
-@end
 
 
 int main() {
@@ -82,6 +100,7 @@ int main() {
   
   Person* person = [[Person alloc] initWithFirstName:@"Gildong" lastName:@"Hong"];
   NSLog(@"%@", person.fullName);
+  [person foo];
   
   person.fullName = @"Soonsin, Lee";
   NSLog(@"%@", person.fullName);
