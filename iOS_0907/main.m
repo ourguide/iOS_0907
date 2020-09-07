e66cf4ade6ffca721ca8c099e5876f4786efb6e5
Chansik Yun <chansigi@gmail.com>
2020-09-07 10:12:54 +0900

    .

--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -31,7 +31,7 @@
 
 // self에 대한 초기화는 초기화함수에서 이루어집니다.
 - (id)init {
-  // 1. 부모의 초기화 메소드를 명시적으로 호출합니다.
+  // 1. 부모의 '지정' 초기화 메소드를 명시적으로 호출합니다.
   self = [super init];
   
   // 2. 부모의 초기화 메소드가 실패하지 않았다면, 자신의 필드를 초기화한다.
@@ -43,6 +43,7 @@
   return self;
 }
 
+// 지정 초기화 메소드 - Designated Initializer
 - (id)initWithColor:(int)color {
   self = [super init];
   
@@ -55,8 +56,30 @@
 
 @end
 
+@interface Truck : Car {
+  int _speed;
+}
+
+- (id)initWithSpeed:(int)speed color:(int)color;
+
+
+@end
+
+@implementation Truck
+
+- (id)initWithSpeed:(int)speed color:(int)color {
+  self = [super initWithColor:color];
+  if (self) {
+    _speed = speed;
+  }
+  return self;
+}
+
+@end
+
+
 // new
-//  1) 객체의 메모리를 할당한다.
+//  1) 객체의 메모리를 할당한다. - alloc
 //  2) init 함수를 통해 객체의 필드를 초기화한다.
 // * new는 init을 호출하도록 약속되어 있습니다.
 
@@ -65,11 +88,15 @@
 //  => 2단계 생성 패턴
 
 int main() {
-  Car* car = [Car new];
+  // Car* car = [Car new];
+  Car* car = [[Car alloc] init];
   printf("%d\n", [car color]);
   
   Car* car2 = [[Car alloc] initWithColor:100];
-  printf("%d\n", [car2 color]);  
+  printf("%d\n", [car2 color]);
+  
+  Truck* truck = [[Truck alloc] initWithSpeed:42 color:100];
+  
 }
 
 
