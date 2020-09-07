b21771ea53917cc8c1b23afe779c79520db5e59b
Chansik Yun <chansigi@gmail.com>
2020-09-07 11:58:56 +0900

    .

---
+++ iOS_0907/6_참조계수.m
@@ -0,0 +1,56 @@
+// 메모리
+// 1. 객체의 수명을 관리하는 기술
+//  => '참조 계수 기반'으로 객체의 수명을 관리합니다.
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+@end
+
+@implementation Car
+
+- (void)dealloc {
+  printf("Car dealloc!\n");
+  
+  // MRC 에서는 부모의 dealloc을 명시적으로 호출해야 합니다.
+  [super dealloc];
+}
+
+@end
+
+// 2. retainCount
+//  => 현재 객체의 참조 계수를 확인한다.
+//  => MRC에서만 사용할 수 있다.
+//    - retain: 참조 계수를 증가
+//    - release: 참조 계수를 감소
+
+
+// Java vs ObjC
+// 참조 계수를 구현하는 기술
+// - Java(VM): 별도의 런타임이 참조 계수를 관리한다.
+//   => 런타임에 GC가 동작하므로, 성능 저하의 원인이 될 수 있다.
+//   => 런타임에 파악하기 때문에, 거의 제대로 동작한다.
+
+// - Obj / Swift(ARC): 컴파일러가 코드를 분석해서, 참조 계수를 관리하는 코드를 삽입한다.
+//   => 런타임에 별도로 동작하는 것이 아니라, 컴파일 타임에 삽입하기 때문에, 성능 저하가 없다.
+//   => 객체가 더이상 사용되지 않는 시점에 바로 파괴된다.
+//   => 컴파일러가 제대로 파악하지 못하는 코드에 대해선, 누수의 위험성이 있다.
+
+int main() {
+  // 1) 객체 생성시 참조 계수는 1 입니다.
+  Car* p1 = [Car new];
+  printf("Ref count: %ld\n", [p1 retainCount]);
+  
+  // 2) 객체의 포인터 대입 후, 참조 계수는 증가해야 합니다.
+  Car* p2 = p1;
+  [p2 retain];
+  printf("Ref count: %ld\n", [p1 retainCount]);
+  
+  [p1 release];
+  printf("Ref count: %ld\n", [p1 retainCount]);
+  
+  [p2 release];
+  // printf("Ref count: %ld\n", [p2 retainCount]);
+  
+  printf("main end...\n");
+}
--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -1,56 +1,57 @@
-// 메모리
-// 1. 객체의 수명을 관리하는 기술
-//  => '참조 계수 기반'으로 객체의 수명을 관리합니다.
-
 #import <Foundation/Foundation.h>
 
-@interface Car : NSObject
+@interface Car : NSObject {
+  int _red;
+  int _green;
+  int _blue;
+}
+
+- (id)initWithRed:(int)red green:(int)green blue:(int)blue;
+
++ (Car*)blueCar;
++ (Car*)redCar;
+
 @end
 
 @implementation Car
 
-- (void)dealloc {
-  printf("Car dealloc!\n");
-  
-  // MRC 에서는 부모의 dealloc을 명시적으로 호출해야 합니다.
-  [super dealloc];
+// 정적 팩토리 메소드 => 편의 생성자
+//                  : 내부적으로 지정 초기화 메소드를 통해 객체를 편리하게 생성하는 클래스 메소드
++ (Car*)blueCar {
+  return [[Car alloc] initWithRed:0 green:0 blue:255];
+}
+
++ (Car*)redCar {
+  return [[Car alloc] initWithRed:255 green:0 blue:0];
+}
+
+
+
+- (id)initWithRed:(int)red green:(int)green blue:(int)blue {
+  self = [super init];
+  if (self) {
+    _red = red;
+    _green = green;
+    _blue = blue;
+  }
+  return self;
 }
 
 @end
 
-// 2. retainCount
-//  => 현재 객체의 참조 계수를 확인한다.
-//  => MRC에서만 사용할 수 있다.
-//    - retain: 참조 계수를 증가
-//    - release: 참조 계수를 감소
-
-
-// Java vs ObjC
-// 참조 계수를 구현하는 기술
-// - Java(VM): 별도의 런타임이 참조 계수를 관리한다.
-//   => 런타임에 GC가 동작하므로, 성능 저하의 원인이 될 수 있다.
-//   => 런타임에 파악하기 때문에, 거의 제대로 동작한다.
-
-// - Obj / Swift(ARC): 컴파일러가 코드를 분석해서, 참조 계수를 관리하는 코드를 삽입한다.
-//   => 런타임에 별도로 동작하는 것이 아니라, 컴파일 타임에 삽입하기 때문에, 성능 저하가 없다.
-//   => 객체가 더이상 사용되지 않는 시점에 바로 파괴된다.
-//   => 컴파일러가 제대로 파악하지 못하는 코드에 대해선, 누수의 위험성이 있다.
-
 int main() {
-  // 1) 객체 생성시 참조 계수는 1 입니다.
-  Car* p1 = [Car new];
-  printf("Ref count: %ld\n", [p1 retainCount]);
+  Car* blueCar = [Car blueCar];
+  Car* redCar = [Car redCar];
   
-  // 2) 객체의 포인터 대입 후, 참조 계수는 증가해야 합니다.
-  Car* p2 = p1;
-  [p2 retain];
-  printf("Ref count: %ld\n", [p1 retainCount]);
   
-  [p1 release];
-  printf("Ref count: %ld\n", [p1 retainCount]);
+#if 0
+  Car* blueCar = [[Car alloc] initWithRed:0
+                                    green:0
+                                     blue:255];
   
-  [p2 release];
-  // printf("Ref count: %ld\n", [p2 retainCount]);
-  
-  printf("main end...\n");
+  Car* redCar = [[Car alloc] initWithRed:255
+                                   green:0
+                                    blue:0];
+#endif
 }
+
