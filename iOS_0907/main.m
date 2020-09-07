65e9b47db421baa423dd6e33607773add0f7313e
Chansik Yun <chansigi@gmail.com>
2020-09-07 11:02:23 +0900

    .

--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -1,3 +1,36 @@
 // 프로퍼티(Property)
 
 #import <Foundation/Foundation.h>
+
+@interface Car : NSObject {
+  int _color;
+}
+
+// Getter
+- (int)color;
+// Setter
+- (void)setColor:(int)color;
+
+@end
+
+@implementation Car
+
+- (int)color {
+  printf("Getter\n");
+  return _color;
+}
+
+- (void)setColor:(int)color {
+  printf("Setter\n");
+  _color = color;
+}
+
+@end
+
+int main() {
+  Car* car = [[Car alloc] init];
+  
+  [car setColor:100];
+  
+  printf("%d\n", [car color]);
+}
