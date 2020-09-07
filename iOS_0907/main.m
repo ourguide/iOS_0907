fe57fa270a2fe81c0c73cbfaa1167e0ba67b794f
Chansik Yun <chansigi@gmail.com>
2020-09-07 09:04:10 +0900

    .

--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -10,6 +10,11 @@
  
 // Command + B: Build(Compile)
 // Command + R: Build(Compile) + Run
+// https://github.com/ourguide/iOS_0907
+
+// 3. ObjC 의 모든 클래스에는 NS라는 접두가 있습니다.
+//  NS: NextStep
+//  => ObjC 언어는 패키지 또는 네임스페이스같은 심볼 충돌을 막기 위한 언어적 기능이 존재하지 않습니다.
 
 int main() {
   // C의 문자열
