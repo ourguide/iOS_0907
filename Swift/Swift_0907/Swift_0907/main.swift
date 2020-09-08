f65c064442f64db596e695c0f9fb36b029da9334
Chansik Yun <chansigi@gmail.com>
2020-09-08 17:24:34 +0900

    .

--- Swift/Swift_0907/Swift_0907/main.swift
+++ Swift/Swift_0907/Swift_0907/main.swift
@@ -1,47 +1,45 @@
 import Foundation
 
-// Objective C
-//   - Reference Type: 객체
-//   - Built-in Type: int, double, char
-
-// Swift는 모든 것이 객체(object) 입니다.
-//  => 객체 = 속성(property) + 메소드
-//   1) Reference Type - class
-//     : 힙에 생성된다.
-//       ARC(참조 계수)에 의해 수명 관리가 된다.
-
-//   2) Value Type     - struct
-//     : 어떤 컨텍스트에서 생성 했는지에 따라 위치가 결정된다.
-//       스택에 생성할 경우, 메모리 생성 해지의 비용이 거의 없다.
-
-// 구조체(struct)
-struct User2 {
-  var name: String
-  var age: Int
-  
-  init(name: String, age: Int) {
-    self.name = name
-    self.age = age
+// ObjC의 ARC의 정책은 Swift도 동일합니다.
+class Car {
+  // - (void)dealloc
+  // 객체가 파괴되는 시점에 수행되는 블록을 정의할 수 있습니다.
+  deinit {
+    print("Car deinit")
   }
 }
 
-class User {
-  var name: String
-  var age: Int
+var p1: Car? = Car()   // 객체 생성시 참조 계수 - 1
+var p2: Car? = p1      // 참조 변수 대입시 참조 계수 증가 - 2
+
+print("1....")
+p1 = nil               // 2 -> 1
+print("2....")
+p2 = nil               // 1 -> 0  => deinit!
+print("3....")
+
+class Node {
+  unowned var next: Node?
+  // 약한 참조
+  //  - autoniling
+  //  - weak 프로퍼티는 반드시 Optional 이어야 합니다.
+  //  0bjC
+  //   weak               -> weak
+  //   unsafe_unretained  -> unowned
   
-  init(name: String, age: Int) {
-    self.name = name
-    self.age = age
+  deinit {
+    print("Node 객체 파괴")
   }
 }
 
-// 클래스 객체는 let으로 지정되도, 불변이 아니다.
-let cUser = User(name: "Tom", age: 42)
-cUser.name = "Bob"
+var n1: Node? = Node()
+var n2: Node? = Node()
+n1?.next = n2
+n2?.next = n1
+
+n1 = nil
+n2 = nil
 
 
-// 구조체 객체는 let으로 지정되면, 불변이다.
-let sUser = User2(name: "Tom", age: 42)
-// sUser.name = "Bob"
-let arr1: [String] = [];  // 불변
-var arr2: [String] = [];  // 가변
+
+
