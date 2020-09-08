ec0842fb93087a63f1ab0b7966e249dd2496c9a3
Chansik Yun <chansigi@gmail.com>
2020-09-08 13:46:52 +0900

    .

--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -2,19 +2,37 @@
 
 // Collection을 열거하는 방법.
 // 1. for
+// 2. NSEnumerator(= Iterator)
 int main() {
   NSArray* arr = @[ @10, @20, @30 ];
+  
+#if 0
   for (int i = 0 ; i < arr.count ; ++i) {
     NSLog(@"%@", arr[i]);
+  }
+#endif
+  
+  NSEnumerator* enumerator = [arr objectEnumerator];
+  id object;
+  while ((object = [enumerator nextObject]) != nil) {
+    NSLog(@"%@", object);
   }
   
   NSDictionary* dic = @{
     @"name": @"Tom",
     @"age": @42
   };
+  
+  enumerator = [dic keyEnumerator];
+  while ((object = [enumerator nextObject]) != nil) {
+    NSLog(@"%@ -> %@", object, dic[object]);
+  }
+  
+#if 0
   NSArray* keys = [dic allKeys];
   for (int i = 0 ; i < keys.count; ++i) {
     id key = keys[i];
     NSLog(@"%@ -> %@", key, dic[key]);
   }
+#endif
 }
