f791d90a82f947c1528602d4e478b746e2757d9a
Chansik Yun <chansigi@gmail.com>
2020-09-08 14:13:20 +0900

    .

---
+++ iOS_0907/24_KVC.m
@@ -0,0 +1,35 @@
+#import <Foundation/Foundation.h>
+
+// KVC
+//  : Key-Value Coding
+//  - 프로퍼티의 이름을 키 값으로 해서, 객체에서 사전처럼 값을 읽거나 변경할 수 있는 기능.
+
+@interface Person : NSObject
+
+@property(strong, nonatomic) NSString* name;
+@property(assign, nonatomic) int age;
+// ...
+
+@end
+
+@implementation Person
+@end
+
+int main() {
+  Person* p = [Person new];
+  p.name = @"Tom";
+  p.age = 42;
+  
+  //--------
+  // 사용자로부터 입력을 받는다.
+  NSString* input = @"age";
+  NSLog(@"%@", [p valueForKey:input]);
+  
+  // KVC가 없다면...
+  if ([input isEqualToString:@"name"]) {
+    NSLog(@"name: %@", p.name);
+  } else if ([input isEqualToString:@"age"]) {
+    NSLog(@"age: %d", p.age);
+  }
+
+}
