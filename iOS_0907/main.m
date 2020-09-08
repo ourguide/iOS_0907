053f80905ca7984e97fbf695ca84a82c5cb2f40b
Chansik Yun <chansigi@gmail.com>
2020-09-08 12:21:01 +0900

    .

--- iOS_0907/main.m
+++ iOS_0907/main.m
@@ -1,6 +1,12 @@
 #import <Foundation/Foundation.h>
 
-@interface Person : NSObject
+// 해결 방법
+// 1. 복사
+//  - NSCopying
+//  - copyWithZone:
+//    alloc -> allocWithZone:
+
+@interface Person : NSObject<NSCopying>
 @property(strong, nonatomic) NSString* name;
 @property(assign, nonatomic) int age;
 
@@ -9,12 +15,20 @@
 @end
 
 @interface Company : NSObject
-@property(strong, nonatomic) Person* person;
+// @property(strong, nonatomic) Person* person;
+@property(copy, nonatomic) Person* person;
+// setter에서 복사본을 생성하고, 소유합니다.
 
 - (instancetype)initWithPerson:(Person*)person;
 @end
 
 @implementation Person
+
+// NSCopying
+- (id)copyWithZone:(NSZone *)zone {
+  // zone에 객체를 생성하고, 초기화하면 됩니다.
+  return [[Person allocWithZone:zone] initWithName:_name age:_age];  // !!!
+}
 
 - (NSString *)description {
   return [@{
@@ -38,7 +52,8 @@
 - (instancetype)initWithPerson:(Person*)person {
   self = [super init];
   if (self) {
-    _person = person;
+    // _person = person;
+    _person = [person copy]; // !!
   }
   return self;
 }
@@ -48,14 +63,15 @@
     @"person": _person
   } description];
 }
-
 @end
-
 
 int main() {
   Person *person = [[Person alloc] initWithName:@"Tom" age:42];
   NSLog(@"%@", person);
   
   Company *company = [[Company alloc] initWithPerson:person];
+  //----
+  person.name = @"Bob";
+  //----
   NSLog(@"%@", company);
 }
