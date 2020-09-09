580ec3dcc0ea24ce7de596c38e74bd31df26174c
Chansik Yun <chansigi@gmail.com>
2020-09-09 14:46:05 +0900

    .

--- ObjC/SampleApp/SampleApp/AppDelegate.h
+++ ObjC/SampleApp/SampleApp/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  SampleApp
-//
-//  Created by Chansik Yun on 2020/09/09.
-//  Copyright © 2020 Chansik Yun. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
