49676b6e19b0974d2ceb73fa8cd8c81a9a0997a8
Chansik Yun <chansigi@gmail.com>
2020-09-09 14:56:38 +0900

    .

--- ObjC/SampleApp/SampleApp/AppDelegate.m
+++ ObjC/SampleApp/SampleApp/AppDelegate.m
@@ -1,15 +1,27 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
-
-@end
-
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   // Override point for customization after application launch.
+  
+  printf("App이 시작되었음\n");
+  
   return YES;
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+  printf("App이 종료될 예정임\n");
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  printf("활성 상태가 되었음..\n");
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+  printf("비활성 상태가 되었음..\n");
 }
 
 @end
--- ObjC/SampleApp/SampleApp/main.m
+++ ObjC/SampleApp/SampleApp/main.m
@@ -1,19 +1,31 @@
-//
-//  main.m
-//  SampleApp
-//
-//  Created by Chansik Yun on 2020/09/09.
-//  Copyright © 2020 Chansik Yun. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+//int main(int argc, char * argv[]) {
+//  NSString * appDelegateClassName;
+//  @autoreleasepool {
+//      // Setup code that might create autoreleased objects goes here.
+//      appDelegateClassName = NSStringFromClass([AppDelegate class]);
+//  }
+//  return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+//}
+
+// 1. iOS도 시작도 main 입니다.
+// 2. UIApplicationMain
+//    1) info.plist 로부터 앱의 기본 설정을 읽어들여서 설정합니다.
+//    2) UIApplication 객체 생성
+//    3) 앱의 상태에 변화에 대응하기 위한 사용자 정의 객체를 생성한다.
+//         - 사용자로부터 전달받는 클래스의 이름을 통해 Class 객체 생성(NSClassForName)
+//         - Class객체를 이용해서 사용자 정의 객체를 생성한다.
+//         - UIApplication 객체의 delegate로 AppDelegate를 등록합니다.
+
 int main(int argc, char * argv[]) {
-  NSString * appDelegateClassName;
+  printf("Hello, iOS\n");
+
+  // main에서 auto release pool을 하나 생성해야 합니다.
   @autoreleasepool {
-      // Setup code that might create autoreleased objects goes here.
-      appDelegateClassName = NSStringFromClass([AppDelegate class]);
+  
+    return UIApplicationMain(argc, argv, nil, @"AppDelegate");
   }
-  return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
