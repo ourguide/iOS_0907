0cce1de92d2685d39442d3fc3ef367533343d01e
Chansik Yun <chansigi@gmail.com>
2020-09-09 17:05:21 +0900

    .

--- ObjC/2_UI기본/SampleApp/ViewController.h
+++ ObjC/2_UI기본/SampleApp/ViewController.h
@@ -2,4 +2,20 @@
 
 @interface ViewController : UIViewController
 
+// #define IBOutlet
+
+// Outlet - 참조
+// Action - 동작에 함수를 정의(Target-Action)
+@property (strong, nonatomic) IBOutlet UILabel *countLabel;
+@property (strong, nonatomic) IBOutlet UIButton *addButton;
+
+// UIView: view
+//        - label(2)
+//        - button(2)
+
+// UIViewController
+//     view
+//        - countLabel(strong)
+//        - addButton
+
 @end
--- ObjC/2_UI기본/SampleApp/ViewController.m
+++ ObjC/2_UI기본/SampleApp/ViewController.m
@@ -1,10 +1,27 @@
 #import "ViewController.h"
 
+@interface ViewController ()
+@property(assign, nonatomic) int count;
+@end
+
+
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+  [super viewDidLoad];
+  // Do any additional setup after loading the view from its nib.
+  
+  self.count = 0;
+  
+  [_addButton addTarget:self action:@selector(touched) forControlEvents:UIControlEventTouchUpInside];
+  
 }
+
+- (void)touched {
+  self.count++;
+  
+  [_countLabel setText:[NSString stringWithFormat:@"%d", _count]];
+}
+
 
 @end
