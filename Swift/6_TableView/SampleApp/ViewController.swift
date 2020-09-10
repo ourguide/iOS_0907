23ec997fcea04f0d34e4ce7c5a34fa50781fc4db
Chansik Yun <chansigi@gmail.com>
2020-09-10 15:37:34 +0900

    .

--- Swift/6_TableView/SampleApp/ViewController.swift
+++ Swift/6_TableView/SampleApp/ViewController.swift
@@ -5,5 +5,17 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
+  
+  // XIB
+  @IBAction func tableView1(_ sender: Any) {
+    let controller = TableController1()
+    controller.modalPresentationStyle = .fullScreen
+    present(controller, animated: true)
+  }
+  
+  // Storyboard
+  @IBAction func tableView2(_ sender: Any) {
+  }
+  
 }
 
