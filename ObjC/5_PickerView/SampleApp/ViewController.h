7cad1d41e5ab566d209c9718cdd6a85c3e476d6b
Chansik Yun <chansigi@gmail.com>
2020-09-10 14:55:31 +0900

    .

--- ObjC/5_PickerView/SampleApp/ViewController.h
+++ ObjC/5_PickerView/SampleApp/ViewController.h
@@ -1,6 +1,8 @@
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIPickerViewDelegate, UIPickerViewDataSource> {
+  NSArray* _animals;
+}
 
 @property (strong, nonatomic) IBOutlet UIPickerView *pickerView;
 @property (strong, nonatomic) IBOutlet UILabel *nameLabel;
--- ObjC/5_PickerView/SampleApp/ViewController.m
+++ ObjC/5_PickerView/SampleApp/ViewController.m
@@ -2,9 +2,46 @@
 
 @implementation ViewController
 
+// Delegate Pattern
+//  - Protocol
+
+//  UIPickerView
+//    - UIPickerViewDelegate
+//    - UIPickerViewDataSource
+
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+  [super viewDidLoad];
+  // Do any additional setup after loading the view from its nib.
+  
+  _animals = @[ @"사자", @"코끼리", @"뱀", @"곰", @"늑대" ];
+  
+  _pickerView.delegate = self;
+  _pickerView.dataSource = self;
+}
+
+#pragma mark - UIPickerViewDataSource
+
+// returns the number of 'columns' to display.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+  return 1;
+}
+
+// returns the # of rows in each component..
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+  return _animals.count;
+}
+
+#pragma mark - UIPickerViewDelegate
+
+- (NSString *)pickerView:(UIPickerView *)pickerView
+             titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+  return _animals[row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView
+      didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+  
+  _nameLabel.text = _animals[row];
 }
 
 @end
