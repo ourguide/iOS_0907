d4e534e77b1abc06a14f1aa6cf184b92cd911fed
Chansik Yun <chansigi@gmail.com>
2020-09-10 14:41:14 +0900

    .

---
+++ ObjC/5_PickerView/SampleApp/ViewController.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIPickerView *pickerView;
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+
+@end
---
+++ ObjC/5_PickerView/SampleApp/ViewController.m
@@ -0,0 +1,10 @@
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
