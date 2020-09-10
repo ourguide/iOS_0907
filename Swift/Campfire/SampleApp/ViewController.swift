aa3d062119dd1d10d090b53d9d2414b82794aa78
Chansik Yun <chansigi@gmail.com>
2020-09-10 09:55:22 +0900

    .

--- Swift/Campfire/SampleApp/ViewController.swift
+++ Swift/Campfire/SampleApp/ViewController.swift
@@ -2,9 +2,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var imageView: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    var images = [UIImage]()
+    for i in 1...17 {
+      let filename = String(format: "campFire%02d.gif", i)
+      if let image = UIImage(named: filename) {
+        images.append(image)
+      }
+    }
+    
+    imageView.animationImages = images
+  }
+  
+  @IBAction func play(_ sender: Any) {
+    imageView.startAnimating()
+  }
+  
+  @IBAction func pause(_ sender: Any) {
+    imageView.stopAnimating()
+  }
+  
+  @IBAction func valueChanged(_ sender: UISlider) {
   }
 }
 
