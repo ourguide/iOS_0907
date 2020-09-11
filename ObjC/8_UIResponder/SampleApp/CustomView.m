e7fa4a85e1339348cf3444ca465a60ac13b99c2e
Chansik Yun <chansigi@gmail.com>
2020-09-11 13:40:00 +0900

    .

---
+++ ObjC/8_UIResponder/SampleApp/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  SampleApp
+//
+//  Created by Chansik Yun on 2020/09/11.
+//  Copyright © 2020 Chansik Yun. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
