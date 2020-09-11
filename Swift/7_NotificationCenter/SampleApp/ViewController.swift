5f6ab4f9ac58d2ea59578b7a70213685ff94cc44
Chansik Yun <chansigi@gmail.com>
2020-09-11 11:18:31 +0900

    .

--- Swift/7_NotificationCenter/SampleApp/ViewController.swift
+++ Swift/7_NotificationCenter/SampleApp/ViewController.swift
@@ -9,19 +9,43 @@ class ViewController: UIViewController {
   
   @IBOutlet var bottomConstraint: NSLayoutConstraint!
   
+  // 비동기(Async)
+  //  => 결과를 처리하기 위해서 '콜백'을 등록한다.
+  //  => 비동기 루틴이 어떤 컨텍스트에서 수행되는지 설정할 수 있다.
+  //   - UI에 대한 처리 반드시 main 스레드에서 수행이 되어야 합니다.
+  //   - 그 외 작업은 다른 스레드에서 처리하는 것이 가능합니다.
+  //    : DispatchQueue
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     
     let notificationCenter = NotificationCenter.default
-    notificationCenter.addObserver(self,
-                                   selector: #selector(keyboardWillShow),
-                                   name: UIResponder.keyboardWillShowNotification,
-                                   object: nil)
-    notificationCenter.addObserver(self,
-                                   selector: #selector(keyboardWillHide),
-                                   name: UIResponder.keyboardWillHideNotification,
-                                   object: nil)
+    
+    notificationCenter.addObserver(forName: UIResponder.keyboardWillShowNotification,
+                                   object: nil,
+                                   queue: .main) { notification in
+                                    
+      let keyboardHeight = self.softKeyboardHeight(notification)
+      self.bottomConstraint.constant = 32 + keyboardHeight
+    }
+    
+    notificationCenter.addObserver(forName: UIResponder.keyboardWillHideNotification,
+                                   object: nil,
+                                   queue: .main) { _ in
+      self.bottomConstraint.constant = 32
+    }
+    
+    /*
+     notificationCenter.addObserver(self,
+                                    selector: #selector(keyboardWillShow),
+                                    name: UIResponder.keyboardWillShowNotification,
+                                    object: nil)
+     notificationCenter.addObserver(self,
+                                    selector: #selector(keyboardWillHide),
+                                    name: UIResponder.keyboardWillHideNotification,
+                                    object: nil)
+     */
   }
   
   @objc func keyboardWillShow(_ notification: Notification) {
@@ -43,8 +67,6 @@ class ViewController: UIViewController {
     
     return 0
   }
-  
-  
   
   override func touchesEnded(_ touches: Set<UITouch>,
                              with event: UIEvent?)
