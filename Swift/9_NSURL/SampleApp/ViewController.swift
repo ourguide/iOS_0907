474373db29319d8e84c1a954e389da9a1b4b2cad
Chansik Yun <chansigi@gmail.com>
2020-09-11 14:18:58 +0900

    .

---
+++ Swift/9_NSURL/SampleApp/ViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet var imageView: UIImageView!
+  
+  
+  @IBAction func click1(_ sender: Any) {
+  }
+  @IBAction func click2(_ sender: Any) {
+  }
+  @IBAction func click3(_ sender: Any) {
+  }
+  @IBAction func click4(_ sender: Any) {
+  }
+  @IBAction func click5(_ sender: Any) {
+  }
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+}
+
