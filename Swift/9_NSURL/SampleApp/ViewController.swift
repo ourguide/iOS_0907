2650bfe53e41a300ee872163cb3d9e38b876f861
Chansik Yun <chansigi@gmail.com>
2020-09-11 14:37:32 +0900

    .

--- Swift/9_NSURL/SampleApp/ViewController.swift
+++ Swift/9_NSURL/SampleApp/ViewController.swift
@@ -2,11 +2,28 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  static let imageURL = "https://www.logo.wine/a/logo/Swift_(programming_language)/Swift_(programming_language)-Logo.wine.svg"
+  // https://www.logo.wine/a/logo/Swift_(programming_language)/Swift_(programming_language)-Logo.wine.svg
+  
   @IBOutlet var imageView: UIImageView!
   
-  
   @IBAction func click1(_ sender: Any) {
+    guard let url = URL(string: ViewController.imageURL) else {
+      return
+    }
+    
+    guard let data = try? Data(contentsOf: url) else {
+      return
+    }
+    
+    guard let image = UIImage(data: data) else {
+      return
+    }
+    
+    imageView.image = image
   }
+  
+  
   @IBAction func click2(_ sender: Any) {
   }
   @IBAction func click3(_ sender: Any) {
